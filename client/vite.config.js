84a5245f63265751b7c56ca0e92eaac8f11a0962
plinio8899 <plinio8899@gmail.com>
2023-04-05 22:15:42 -0500

    Se añade un compresor de css

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'css',
+    rollupOptions: {
+      plugins: [
+        // ...otros complementos que estés utilizando
+        minify(),
+      ],
+    },
+  },
 })
