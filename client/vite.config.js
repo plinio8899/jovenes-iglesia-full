ea4f872ea9875709786c19b4bcaa5c6da85a24c6
plinio8899 <plinio8899@gmail.com>
2023-04-08 00:16:34 -0500

    Se arregla el bug de vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'css',
-    rollupOptions: {
-      plugins: [
-        // ...otros complementos que estés utilizando
-        new CssMinimizerPlugin().minimize,
-      ],
-    },
-  },
 })
