c4c0dfb366dd82da625a6ee2ab2a3d64334127a2
plinio8899 <plinio8899@gmail.com>
2023-04-10 22:02:53 -0500

    Se añade nuevamente el optimix css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'css',
+    rollupOptions: {
+      plugins: [
+        // ...otros complementos que estés utilizando
+        new CssMinimizerPlugin().minimize,
+      ],
+    },
+  },
 })
